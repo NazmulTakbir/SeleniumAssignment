7f946440f7c655144d6f4fbc2fc80123b4a61728
Nazmul Takbir <nazmultakbir98@gmail.com>
2021-07-20 21:24:41 +0600

    Register and Login Automated

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,51 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
+    private static WebDriver driver;
+
     public static void main(String[] args) {
-        String chromeDriver = "E:\\Main\\Semesters\\3_1\\CSE 307 - Software Engineering\\Assignment\\SeleniumProject\\chromedriver.exe";
-        System.setProperty("webdriver.chrome.driver", chromeDriver);
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://moodle.cse.buet.ac.bd/");
         try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
+            String chromeDriver = "E:\\Main\\Semesters\\3_1\\CSE 307 - Software Engineering\\Assignment\\SeleniumProject\\chromedriver.exe";
+            System.setProperty("webdriver.chrome.driver", chromeDriver);
+            driver = new ChromeDriver();
+            register("1705103@ugrad.cse.buet.ac.bd", "Nazmul Takbir", "22", "8801727498589", "1234", "1234");
+            login("1705103@ugrad.cse.buet.ac.bd", "1234");
+        } catch (Exception e) {
             System.out.println(e);
         }
-        driver.quit();
+    }
+
+    private static void login(String email, String password) throws Exception {
+        driver.get("http://127.0.0.1:8000/");
+        driver.manage().window().maximize();
+        Thread.sleep(1000);
+        driver.findElement(By.id("id_email")).sendKeys(email);
+        Thread.sleep(1000);
+        driver.findElement(By.id("id_password")).sendKeys(password);
+        Thread.sleep(1000);
+        driver.findElement(By.id("id_submit")).click();
+    }
+
+    private static void register(String email, String name, String age, String phoneNUmber, String password, String password2) throws Exception {
+        driver.get("http://127.0.0.1:8000/");
+        driver.manage().window().maximize();
+        Thread.sleep(1000);
+        driver.findElement(By.id("id_register")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.id("id_email")).sendKeys(email);
+        Thread.sleep(1000);
+        driver.findElement(By.id("id_name")).sendKeys(name);
+        Thread.sleep(1000);
+        driver.findElement(By.id("id_age")).sendKeys(age);
+        Thread.sleep(1000);
+        driver.findElement(By.id("id_phone")).sendKeys(phoneNUmber);
+        Thread.sleep(1000);
+        driver.findElement(By.id("id_password")).sendKeys(password);
+        Thread.sleep(1000);
+        driver.findElement(By.id("id_confirm_password")).sendKeys(password2);
+        Thread.sleep(1000);
+        driver.findElement(By.id("id_register")).click();
     }
 }
