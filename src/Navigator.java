3ebbb03d68a3940c6849d890d07d7385ec0778d2
Nazmul Takbir <nazmultakbir98@gmail.com>
2021-07-21 04:17:21 +0600

    Register Automation Partially Done

--- src/Navigator.java
+++ src/Navigator.java
@@ -8,7 +8,7 @@ import java.nio.file.Paths;
 
 public class Navigator {
     private WebDriver driver;
-    private int time = 1000;
+    private int time = 200;
     private boolean pause;
     private String webBrowser;
 
@@ -34,7 +34,14 @@ public class Navigator {
         else if( webBrowser.equalsIgnoreCase("firefox") ) driver = new FirefoxDriver();
     }
 
-    private void quitBrowser() { driver.quit(); }
+    private void quitBrowser() {
+        try {
+            Thread.sleep(time*5);
+            driver.quit();
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
 
     public boolean login(String email, String password, String expectedText)  {
         boolean testResult = false;
@@ -62,25 +69,43 @@ public class Navigator {
         return testResult;
     }
 
-    public void register(String email, String name, String age, String phoneNUmber, String password, String password2) throws Exception {
-        driver.get("http://127.0.0.1:8000/register/");
-        driver.manage().window().maximize();
-        if(pause) Thread.sleep(time);
-        driver.findElement(By.id("id_register")).click();
-        if(pause) Thread.sleep(time);
-        driver.findElement(By.id("id_email")).sendKeys(email);
-        if(pause) Thread.sleep(time);
-        driver.findElement(By.id("id_name")).sendKeys(name);
-        if(pause) Thread.sleep(time);
-        driver.findElement(By.id("id_age")).sendKeys(age);
-        if(pause) Thread.sleep(time);
-        driver.findElement(By.id("id_phone")).sendKeys(phoneNUmber);
-        if(pause) Thread.sleep(time);
-        driver.findElement(By.id("id_password")).sendKeys(password);
-        if(pause) Thread.sleep(time);
-        driver.findElement(By.id("id_confirm_password")).sendKeys(password2);
-        if(pause) Thread.sleep(time);
-        driver.findElement(By.id("id_register")).click();
-        if(pause) Thread.sleep(time);
+    public boolean register(String email, String name, String age, String phoneNUmber, String password, String confirmPassword, String expectedText) {
+        boolean testResult = false;
+        try {
+            startBrowser();
+            driver.get("http://127.0.0.1:8000/register/");
+            driver.manage().window().maximize();
+            if(pause) Thread.sleep(time);
+            driver.findElement(By.id("email")).sendKeys(email);
+            if(pause) Thread.sleep(time);
+            driver.findElement(By.id("name")).sendKeys(name);
+            if(pause) Thread.sleep(time);
+            driver.findElement(By.id("age")).sendKeys(age);
+            if(pause) Thread.sleep(time);
+            driver.findElement(By.id("phone")).sendKeys(phoneNUmber);
+            if(pause) Thread.sleep(time);
+            driver.findElement(By.id("password")).sendKeys(password);
+            if(pause) Thread.sleep(time);
+            driver.findElement(By.id("confirm_password")).sendKeys(confirmPassword);
+            if(pause) Thread.sleep(time);
+            driver.findElement(By.id("id_register")).click();
+            if(pause) Thread.sleep(time);
+            testResult = driver.getPageSource().toLowerCase().contains(expectedText.toLowerCase());
+            if( !testResult ) {
+                String errorMessage = driver.findElement(By.id("email")).getAttribute("validationMessage");
+                errorMessage += driver.findElement(By.id("name")).getAttribute("validationMessage");
+                errorMessage += driver.findElement(By.id("age")).getAttribute("validationMessage");
+                errorMessage += driver.findElement(By.id("phone")).getAttribute("validationMessage");
+                errorMessage += driver.findElement(By.id("password")).getAttribute("validationMessage");
+                errorMessage += driver.findElement(By.id("confirm_password")).getAttribute("validationMessage");
+                System.out.println(errorMessage);
+                System.out.println(expectedText);
+                testResult = errorMessage.toLowerCase().contains(expectedText.toLowerCase());
+            }
+            quitBrowser();
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+        return testResult;
     }
 }
